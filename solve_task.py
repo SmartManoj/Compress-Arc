c2c4ef7e287253480a1b3a3e14858f4ebcedafab
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-15 21:04:35 +0530

    update path

--- solve_task.py
+++ solve_task.py
@@ -11,10 +11,10 @@ import traceback
 import numpy as np
 import torch
 
-sys.path.append('/kaggle/input/darkagicompressarc')
+sys.path.append('/kaggle/usr/lib/compressarc_source/Compress-Arc')
 
 # This little block of code does "import preprocessing" but avoids a name collision with another module
-module_path = "/kaggle/input/darkagicompressarc/preprocessing.py"
+module_path = "/kaggle/usr/lib/compressarc_source/Compress-Arc/preprocessing.py"
 module_name = "preprocessing"
 spec = importlib.util.spec_from_file_location(module_name, module_path)
 preprocessing = importlib.util.module_from_spec(spec)
